f8c1b7e017d59c9df0526db6bf4f98e425305924
4234234545Giahuy4234234545Giahuy <95902552+4234234545Giahuy4234234545Giahuy@users.noreply.github.com>
2021-12-10 13:42:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Tui gất xin chào bà nhaaaaa🖐🖐🖐',
+    introDesc: `Tui có vài điều này muốn hỏi bà nè,  Bà nhớ phải trả lời thật lòng nhoaaaaaaaa`,
+    btnIntro: 'OK tỷ luôn',
+    title: 'Chẳng biết có phải bà cũng cho tui cơ hội bên bà rồi phải hông ? 😙',
+    desc: 'My Huyen không trả lời hoặc thoát ra, tức là Huyen thích Gia Hy đó nha :> ',
+    btnYes: 'Có, em cũng thích Gia Hy ó 💖💖💖',
+    btnNo: '💔 Điên à...! 
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Cho tui biết ở đây nè',
+    btnReply: 'Gửi đi tui chờ nè  ! 💌💌',
+    reply: 'Tại vì Gia Hy đẹp trai, hát hay, dễ thương... hí hí  😊😊',
+    mess: 'Tui biết mà, Tui  cũng thích em lắm 🥰. Love you 3000 😘😘',
+    messDesc: '💬💬 Ib cho tui nha tui sẽ tỏ tình với bà ngay sau tin nhắn này á. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/huy.lamgia.247/'
+}
